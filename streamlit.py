3cb1e7421a8497e634a2b1b2e6836c1d22b09925
AkshayVemulapalli <55768030+AkshayVemulapalli@users.noreply.github.com>
2023-10-21 18:22:43 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -6,7 +6,7 @@ import numpy as np
 # Create a random dataframe for demonstration purposes
 df = pd.DataFrame({
   'x': range(1, 101),
-  'y': np.random.randint(1, 101)
+  'y': np.random.randint(1, 101, size=100)
 })
 
 st.title('Simple Line Chart')
