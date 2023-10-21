ba02c3801f9ee5fc28d40e04fbc2d1c5aed1133c
AkshayVemulapalli <55768030+AkshayVemulapalli@users.noreply.github.com>
2023-10-21 18:29:28 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,10 +1,3 @@
 import streamlit as st
 
-import pandas as pd
-import numpy as np
-
-# Create a random dataframe for demonstration purposes
-df = pd.DataFrame({
-  'x': range(1, 101),
-  'y': [
-st.line_chart(df.y)
+st.write("hello")
