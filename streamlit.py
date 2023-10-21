a199713c00525963893fd5278f51d3e49d4a0c00
AkshayVemulapalli <55768030+AkshayVemulapalli@users.noreply.github.com>
2023-10-21 18:30:30 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,3 +1,11 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+# Create a random dataframe for demonstration purposes
+df = pd.DataFrame({
+  'x': range(1, 101),
+  'y': [int(np.round(np.sin(0.1 * x))) for x in range(100)]
+})
 
-st.write("hello")
+st.title('Simple Line Chart')
+st.line_chart([int(np.round(np.sin(0.1 * x))) for x in range(100)])
