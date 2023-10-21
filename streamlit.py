c271841a416beb13d9f0b7922a8515050258e1ba
AkshayVemulapalli <55768030+AkshayVemulapalli@users.noreply.github.com>
2023-10-21 17:32:45 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,4 +1,14 @@
 import streamlit as st
 
-st.write("# Hello")
-st.write("My name is Akshay.")
+import pandas as pd
+import numpy as np
+
+# Create a random dataframe for demonstration purposes
+df = pd.DataFrame({
+  'x': range(1, 101),
+  'y': np.random.randn(100).cumsum()
+})
+
+st.title('Simple Line Chart')
+
+st.line_chart(df.y)
